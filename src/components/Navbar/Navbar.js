42dffb62437af0bf910cfd5ca125fb1a19763268
Novan Junaedi <junaedinovan98@gmail.com>
2021-02-21 22:17:36 +0700

    change href logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => (
   <section>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="!#">
+        <a className="navbar-brand" href="#top">
           <img src={Logo} width="180" alt="Novan Junaedi" />
         </a>
         <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
