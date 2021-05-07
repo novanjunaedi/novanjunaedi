2f2fcbac0ec6b4a24d440cc39f9b8fdccfa5fb33
Novan Junaedi <junaedinovan98@gmail.com>
2021-05-07 08:10:16 +0700

    add lazy loading in notfound component

--- src/pages/Error/Errorpage.js
+++ src/pages/Error/Errorpage.js
@@ -8,7 +8,7 @@ const Errorpage = () => (
       <div className="d-flex justify-content-center align-items-center vh-100">
         <div className="row">
           <div className="col text-center">
-            <img className="mx-auto d-block" style={{ height: 200, width: 'auto' }} src={notFoundImg} alt="You are Offline" />
+            <img className="mx-auto d-block" style={{ height: 200, width: 'auto' }} src={notFoundImg} loading="lazy" alt="404 Not Found" />
             <p className="lead text-info mb-5">Stop there buddy, you&apos;re lost.</p>
             <NavLink activeClassName="btn-outline-info" className="btn px-5 py-3 rounded-pill" to="/">Back</NavLink>
           </div>
